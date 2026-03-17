692e57f688960bcdbdade3234480b39d53071114
Pivick T. Lorenzo <pivicklorenzo9@gmail.com>
2026-03-17 17:35:07 +0800

    u

--- src/pages/BookAppointment.tsx
+++ src/pages/BookAppointment.tsx
@@ -91,7 +91,7 @@ const BookAppointment = () => {
   const [ocrBack, setOcrBack] = useState("");
   const [idStatusFront, setIdStatusFront] = useState<IDStatus>("idle");
   const [idStatusBack, setIdStatusBack] = useState<IDStatus>("idle");
-  const [isOcrLoading, setIsOcrLoading] = useState(false);
+  const [, setIsOcrLoading] = useState(false);
 
   const idOptions = {
     Primary: [
@@ -470,13 +470,6 @@ const BookAppointment = () => {
       );
     } finally {
       setLoading(false);
-    }
-  };
-
-  const handlePhoneChange = (val: string) => {
-    const numbersOnly = val.replace(/\D/g, "");
-    if (numbersOnly.length <= 11) {
-      setPhoneNumber(numbersOnly);
     }
   };
 
@@ -1462,24 +1455,6 @@ const Input = ({
       )}
     </div>
   </div>
-);
-
-// Stub for Terminal Icon missing in some older lucide-react versions
-const Terminal = ({ size }: any) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <polyline points="4 17 10 11 4 5"></polyline>
-    <line x1="12" y1="19" x2="20" y2="19"></line>
-  </svg>
 );
 
 export default BookAppointment;
